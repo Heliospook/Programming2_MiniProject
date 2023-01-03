4fe8e86aa72e3e30dda8854ba1a80b935c4669a9
todorokishotoua15 <todorokishoto.ua15@gmail.com>
2023-01-03 23:31:39 +0530

    added product header file

---
+++ Products.h
@@ -0,0 +1,16 @@
+#ifndef PRODUCTS_H
+#define PRODUCTS_H
+
+#include "Globals.h"
+
+class Products {
+public:
+    virtual Globals::Category get_category();
+    virtual string get_name();
+    virtual string get_product_id();
+    virtual float get_price();
+    virtual int get_quantity();
+    
+};
+
+#endif
